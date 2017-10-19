8744167327c5375a1debd4d462d8a7f49745bc32
NervoyzZz <nervoidaz@yandex.ru>
2017-10-19 21:23:15 +0300

    Improve Character class. Make ShowInfo and Traits Improvements

--- AdFuncFile.py
+++ AdFuncFile.py
@@ -5,121 +5,162 @@ class clCharacter:
     """ Character class
         There are:
         General Params:
-        str chName - name of Character
-        int chHealth, chDamage[int min, int max] - helth of Character and Damage
-        int chMaxPotion, chPotionSlots[int HealthRestore, IncDamage] - max count that Character can carry
-        and Count of each poison types 
-        int chPotionRestore - how much potion can restore points
-        int chGold - gold of Character
-        int chLevel - Level of character
-        int chHitChance - chance to hit the enemy
-        int chAttackSpeed - if you have A*[Enemy]chAttackSpeed then you hit A times
-        int chFindChance - chance to find interesting in others places
-        int chKnowChance - chance to know something interesting from people
-        int chFleeChance - chance to flee from dead figth
-        int chBattleRestore - percent of chHelth restore after every battle
+        str Name - name of Character
+        int Health, chMinDamage, chMaxDamage - health of Character and Damage
+        int MaxPotion, PotionSlots[int HealthRestore, IncDamage] - max count that Character can carry
+        and Count of each poison types
+        int PotionRestore - how much potion can restore points
+        int Gold - gold of Character
+        int Level - Level of character
+        int HitChance - chance to hit the enemy
+        int AttackSpeed - if you have A*[Enemy]chAttackSpeed then you hit A times
+        int FindChance - chance to find interesting in others places
+        int KnowChance - chance to know something interesting from people
+        int FleeChance - chance to flee from dead figth
+        int BattleRestore - percent of chHelth restore after every battle
 
         Every new level player gets 1 point. At the first level player has 3 points
         You can increase next traits:
-        int chVitality - each point increase chHealth by 5
-        int chStrength - each point increase chDamage by 2
-        int chAccuracy - each point increase chHitChance by 5
-        int chLuck - who has more chLuck that smashes enemy the first
-        int chAgility - each point increase chAttackSpeed by 5
-        int chAttention - each point increase chFindChance by 10
-        int chCharisma - each point increase chKnowChance by 10
-        int chIntelligence - each point increase chPotionRestore by 1
-        int chInstinct - each point increase chFleeChance by 10
+        int Vitality - each point increase chHealth by 5
+        int Strength - each point increase chDamage by 2
+        int Accuracy - each point increase chHitChance by 5
+        int Luck - who has more chLuck that smashes enemy the first
+        int Agility - each point increase chAttackSpeed by 5
+        int Attention - each point increase chFindChance by 10
+        int Charisma - each point increase chKnowChance by 10
+        int Intelligence - each point increase chPotionRestore by 1
+        int Instinct - each point increase chFleeChance by 10
 
         Every 10 levels player gets 1 point to spend it to special traits:
-        int chUndead - every point increase chHealth by 20
-        int chCarrier - every point increase chMaxPotions by 2
-        int chQuickHand - every point increase chSpeed by 20
-        int chOnePunch - every point increase chSpeed by 8
-        int chPhysician - every point increase chBattleRestore by 16
-        int chTrader - character gets 1000 Golds    
+        int Undead - every point increase chHealth by 20
+        int Carrier - every point increase chMaxPotions by 2
+        int QuickHand - every point increase chSpeed by 20
+        int OnePunch - every point increase chSpeed by 8
+        int Physician - every point increase chBattleRestore by 16
+        int Trader - character gets 1000 Golds
     """
     # define character params
-    chName = ''
-    chHealth = 50
-    chDamage = [1, 3]
-    chMaxPotion = 5
-    chPotionSlots = [0, 0]
-    chPotionRestore = 1
-    chGold = 100
-    chLevel = 0
-    chHitChance = 50
-    chAttackSpeed = 5
-    chFindChance = 0
-    chKnowChance = 0
-    chFleeChance = 0
-    chBattleRestore = 20    
-    chVitality = 0
-    chStrength = 0
-    chAccuracy = 0
-    chLuck = 0
-    chAgility = 0
-    chAttention = 0
-    chCharisma = 0
-    chIntelligence = 0
-    chInstinct = 0
-    
-    chUndead = 0
-    chCarrier = 0
-    chQuickHand = 0
-    chOnePunch = 0
-    chPhysician = 0
-    chTrader = 0
+    chGeneralParam = {'Name': '', 'Health': 50, 'MinDamage': 1,
+                      'MaxDamage': 3, 'MaxPotion': 5, 'PotionSlots': [0, 0],
+                      'PotionRestore': 1, 'Gold': 100, 'Level': 0,
+                      'HitChance': 50, 'AttackSpeed': 5,
+                      'FindChance': 0, 'KnowChance': 0,
+                      'FleeChance': 0, 'BattleRestore': 20}
+    chCommonTrait = {'Vitality': 0, 'Strength': 0, 'Luck': 0,
+                     'Accuracy': 0, 'Attention': 0, 'Charisma': 0,
+                     'Intelligence': 0, 'Instinct': 0, 'Agility': 0}
+    chSpecialTrait = {'Undead': 0, 'Carrier': 0, 'QuickHand': 0,
+                      'OnePunch': 0, 'Physician': 0, 'Trader': 0}
+
+
 
     def __init__(self, Name):
-        self.chName = Name
+        self.chGeneralParam['Name'] = Name
 
     def ShowInfo(self, Param = 0):
         '''Param - how much info you need
            Param = 0 shows you only General params
            Param = 1 shows you only Character Traits
            Param = 2 show you only Character Special Traits
-           Param = 3 show you every param       
+           Param = 3 show you every param
         '''
         if Param == 0 or Param == 3:
-            print("Name:", self.chName, " "*5, "Level:", self.chLevel, " "*5, "Gold:", self.chGold)
-            print("Health:", self.chHealth)
-            print("Damage:", self.chDamage[0], "-", self.chDamage[1])
-            print("Potions: Healing", str(self.chPotionSlots[0])+"/"+str(self.chMaxPotion)+"; Damage "+str(self.chPotionSlots[1])+"/"+str(self.chMaxPotion))
-            print("Each potion gets +" + str(self.chPotionRestore))
-            print("Attack speed:", self.chAttackSpeed, " "*5, "Hit Chance:", self.chHitChance)
-            print("Chance to know something interesting from people:", self.chKnowChance)
-            print("Chance to find something interesting in the area:", self.chFindChance)
-            print("Chance to flee from figth:", self.chFleeChance)
-            print(str(self.chBattleRestore)+"%"+" will heal after every battle")
+            print("Name:", self.chGeneralParam['Name'], " "*5,
+                  "Level:", self.chGeneralParam['Level'], " "*5,
+                  "Gold:", self.chGeneralParam['Gold'])
+            print("Health:", self.chGeneralParam['Health'])
+            print("Damage:", self.chGeneralParam['MinDamage'],
+                  "-", self.chGeneralParam['MaxDamage'])
+            print("Potions: Healing", str(self.chGeneralParam['PotionSlots'][0])+
+                  "/"+str(self.chGeneralParam['MaxPotion'])+ "; Damage "+
+                  str(self.chGeneralParam['PotionSlots'][1])+"/"+
+                  str(self.chGeneralParam['MaxPotion']))
+            print("Each potion gets +" + str(self.chGeneralParam['PotionRestore']))
+            print("Attack speed:", self.chGeneralParam['AttackSpeed'],
+                  " "*5, "Hit Chance:", self.chGeneralParam['HitChance'])
+            print("Chance to know something interesting from people:",
+                  self.chGeneralParam['KnowChance'])
+            print("Chance to find something interesting in the area:",
+                  self.chGeneralParam['FindChance'])
+            print("Chance to flee from figth:", self.chGeneralParam['FleeChance'])
+            print(str(self.chGeneralParam['BattleRestore'])+"%"+" will heal after every battle")
         if Param == 1 or Param == 3:
-            print(self.chName, "has next traits:")
-            print("Vitality:", self.chVitality)
-            print("Strength:", self.chStrength)
-            print("Accuracy:", self.chAccuracy)
-            print("Luck:", self.chLuck)
-            print("Agility:", self.chAgility)
-            print("Attention:", self.chAttention)
-            print("Charisma:", self.chCharisma)
-            print("Intelligence:", self.chIntelligence)
-            print("Instinct:", self.chInstinct)
+            print(self.chGeneralParam['Name'], "has next traits:")
+            print("Vitality:", self.chCommonTrait['Vitality'])
+            print("Strength:", self.chCommonTrait['Strength'])
+            print("Accuracy:", self.chCommonTrait['Accuracy'])
+            print("Luck:", self.chCommonTrait['Luck'])
+            print("Agility:", self.chCommonTrait['Agility'])
+            print("Attention:", self.chCommonTrait['Attention'])
+            print("Charisma:", self.chCommonTrait['Charisma'])
+            print("Intelligence:", self.chCommonTrait['Intelligence'])
+            print("Instinct:", self.chCommonTrait['Instinct'])
         if Param == 2 or Param == 3:
-            if not self.chUndead and not self.chCarrier and not self.chQuickHand and not self.chOnePunch and not self.chPhysician and not self.chTrader:
-                print(self.chName, "has no special traits.")
+            if (not self.chSpecialTrait['Undead'] and not self.chSpecialTrait['Carrier']
+                and not self.chSpecialTrait['QuickHand'] and not self.chSpecialTrait['OnePunch']
+                and not self.chSpecialTrait['Physician'] and not self.chSpecialTrait['Trader']):
+                print(self.chGeneralParam['Name'], "has no special traits.")
             else:
-                print(self.chName, "has next special traits:")
-            if self.chUndead != 0:
-                print("Undead with", self.chUndead, "power")
-            if self.chCarrier != 0:
-                print("Carrier with", self.chCarrier, "power")
-            if self.chQuickHand != 0:
-                print("Quick Hand with", self.chQuickHand, "power")
-            if self.chOnePunch != 0:
-                print("One Punch with", self.chOnePunch, "power")
-            if self.chPhysician != 0:
-                print("Physician with", self.chPhysician, "power")
-            if self.chTrader != 0:
-                print("Trader with", self.chTrader, "power")
+                print(self.chGeneralParam['Name'], "has next special traits:")
+            if self.chSpecialTrait['Undead'] != 0:
+                print("Undead with", self.chSpecialTrait['Undead'], "power")
+            if self.chSpecialTrait['Carrier'] != 0:
+                print("Carrier with", self.chSpecialTrait['Carrier'], "power")
+            if self.chSpecialTrait['QuickHand'] != 0:
+                print("Quick Hand with", self.chSpecialTrait['QuickHand'], "power")
+            if self.chSpecialTrait['OnePunch'] != 0:
+                print("One Punch with", self.chSpecialTrait['OnePunch'], "power")
+            if self.chSpecialTrait['Physician'] != 0:
+                print("Physician with", self.chSpecialTrait['Physician'], "power")
+            if self.chSpecialTrait['Trader'] != 0:
+                print("Trader with", self.chSpecialTrait['Trader'], "power")
 
+    def chIncreaseCommonTrait(self, CommonTraitKey):
+        ''' Method increase choosen chCommonTrait
+            by CommonTraitKey
+        '''
+        # just a lot of 'if'
+        if CommonTraitKey in self.chCommonTrait.keys():
+            print(CommonTraitKey, 'has been increased!')
+            self.chCommonTrait[CommonTraitKey] += 1
+            if CommonTraitKey == 'Vitality':
+                self.chGeneralParam['Health'] += 5
+            elif CommonTraitKey == 'Strength':
+                self.chGeneralParam['MinDamage'] += 2
+                self.chGeneralParam['MaxDamage'] += 2
+            elif CommonTraitKey == 'Accuracy':
+                self.chGeneralParam['HitChance'] += 5
+            elif CommonTraitKey == 'Attention':
+                self.chGeneralParam['FindChance'] += 10
+            elif CommonTraitKey == 'Charisma':
+                self.chGeneralParam['KnowChance'] += 10
+            elif CommonTraitKey == 'Intelligence':
+                self.chGeneralParam['PotionRestore'] += 1
+            elif CommonTraitKey == 'Instinct':
+                self.chGeneralParam['FleeChance'] += 10
+        else:
+            print("Imposible to improve", CommonTraitKey)
 
-
+    def chIncreaseSpecialTrait(self, SpecialTraitKey):
+        ''' Method increase choosen chSpecialTrait
+            by SpecialTrait
+        '''
+        # if if if if if if ... if
+        if SpecialTraitKey in self.chSpecialTrait.keys():
+            print(SpecialTraitKey, 'has been improve!')
+            self.chSpecialTrait[SpecialTraitKey] += 1
+            if SpecialTraitKey == 'Undead':
+                self.chGeneralParam['Health'] += 20
+            elif SpecialTraitKey == 'Carrier':
+                self.chGeneralParam['MaxPotion'] += 2
+            elif SpecialTraitKey == 'QuickHand':
+                self.chGeneralParam['AttackSpeed'] += 20
+            elif SpecialTraitKey == 'OnePunch':
+                self.chGeneralParam['MinDamage'] += 8
+                self.chGeneralParam['MaxDamage'] += 8
+            elif SpecialTraitKey == 'Physician':
+                self.chGeneralParam['BattleRestore'] += 16
+            elif SpecialTraitKey == 'Trader':
+                self.chGeneralParam['Gold'] += 1000
+        else:
+            print("Imposible to improve", SpecialTraitKey)
