6f32e1105bfae79f964193cabf6077ede044f0b9
Daniil Nepryahin <nervoidaz@yandex.ru>
2017-12-15 15:26:53 +0300

    Balanced weapons price

--- AdConstFile.py
+++ AdConstFile.py
@@ -57,123 +57,123 @@ constNoneArmour = ({'Name': 'None', 'Description': '', 'Price': 0,
 # to add Price to them
 constDaggerWeapon = ({'Name': 'Rusty dagger', 'Description': """It's an
                       old rusty dagger, that has lived his best times.""",
-                      'Price': 100, 'Damage': (1, 5), 'AttackSpeed': 30},
+                      'Price': 200, 'Damage': (1, 5), 'AttackSpeed': 30},
 
                      {'Name': 'Blunt dagger', 'Description': """It's a good dagger,
-                      but it's blunt.""", 'Price': 300, 'Damage': (2, 6),
+                      but it's blunt.""", 'Price': 520, 'Damage': (2, 6),
                       'AttackSpeed': 40},
 
                      {'Name': 'Dagger', 'Description': """It's a good dagger in
-                      good condition.""", 'Price': 600, 'Damage': (2, 7),
+                      good condition.""", 'Price': 688, 'Damage': (2, 7),
                       'AttackSpeed': 40},
 
                      {'Name': 'Sharpened dagger', 'Description': """It's a good
-                      dagger, that was well sharpened.""", 'Price': 1000,
+                      dagger, that was well sharpened.""", 'Price': 1100,
                       'Damage': (3, 8), 'AttackSpeed': 50})
 
 constSwordWeapon = ({'Name': 'Rusty sword', 'Description': """It's an old rusty
-                      sword, that has lived his best times.""", 'Price': 300,
+                      sword, that has lived his best times.""", 'Price': 757,
                       'Damage': (5, 10), 'AttackSpeed': 15},
 
                      {'Name': 'Blunt sword', 'Description': """It's a good sword,
-                      but it's blunt.""", 'Price': 600, 'Damage': (6, 12),
+                      but it's blunt.""", 'Price': 1098, 'Damage': (6, 12),
                       'AttackSpeed': 15},
 
                      {'Name': 'Sword', 'Description': """It's a good sword in
-                      good condition.""", 'Price': 1300, 'Damage': (8, 13),
+                      good condition.""", 'Price': 2426, 'Damage': (8, 13),
                       'AttackSpeed': 25},
 
                      {'Name': 'Sharpened sword', 'Description': """It's a good
-                      sword, that was well sharpened.""", 'Price': 2000,
+                      sword, that was well sharpened.""", 'Price': 3954,
                       'Damage': (9, 15), 'AttackSpeed': 25})
 
 constAxeWeapon = ({'Name': 'Rusty axe', 'Description': """It's an old rusty
                       axe, that has lived his best times. But it's still cool tool
-                      to crack enemies' skulls.""", 'Price': 400,
+                      to crack enemies' skulls.""", 'Price': 1342,
                       'Damage': (7, 11), 'AttackSpeed': 15},
 
                      {'Name': 'Blunt axe', 'Description': """It's a good axe,
                       but it's blunt. It doesn't chop, it cracks!""",
-                      'Price': 800, 'Damage': (7, 13), 'AttackSpeed': 15},
+                      'Price': 1701, 'Damage': (7, 13), 'AttackSpeed': 15},
 
                      {'Name': 'Axe', 'Description': """It's a good axe in
                       good condition, be made in viking traditions.""",
-                      'Price': 1500, 'Damage': (9, 15), 'AttackSpeed': 20},
+                      'Price': 2546, 'Damage': (9, 15), 'AttackSpeed': 20},
 
                      {'Name': 'Sharpened Axe', 'Description': """What can be
-                      better?""", 'Price': 2200, 'Damage': (10, 17),
+                      better?""", 'Price': 3805, 'Damage': (10, 17),
                       'AttackSpeed': 20})
 
 constSpearWeapon = ({'Name': 'Rusty spear', 'Description': """It's an old rusty
                       spear, that has lived his best times. But it's still cool
-                      place for enemies' heads.""", 'Price': 400,
+                      place for enemies' heads.""", 'Price': 1933,
                       'Damage': (8, 13), 'AttackSpeed': 15},
 
                      {'Name': 'Blunt spear', 'Description': """It's a good spear,
                       but it's blunt. Is it matter for spear?!""",
-                      'Price': 900, 'Damage': (8, 15), 'AttackSpeed': 15},
+                      'Price': 2205, 'Damage': (8, 15), 'AttackSpeed': 15},
 
                      {'Name': 'Spear', 'Description': """It's a good spear in
                       good condition. Few people know that vikings had more spears
-                      than axes.""", 'Price': 1700, 'Damage': (10, 15),
+                      than axes.""", 'Price': 3577, 'Damage': (10, 15),
                       'AttackSpeed': 20},
 
                      {'Name': 'Balanced Spear', 'Description': """It's good
                       spear that was well balanced and sharpened.""",
-                      'Price': 2500, 'Damage': (12, 18),
+                      'Price': 6914, 'Damage': (12, 18),
                       'AttackSpeed': 20})
 
 constGreatSwordWeapon = ({'Name': 'Rusty great sword', 'Description': """It's an old rusty
-                          great sword. It's as big as his history.""", 'Price': 600,
+                          great sword. It's as big as his history.""", 'Price': 2004,
                           'Damage': (10, 17), 'AttackSpeed': 10},
 
                          {'Name': 'Blunt great sword', 'Description': """It's a good great
                           sword. This weapon has no flaw, except his blunt edges.""",
-                          'Price': 1100, 'Damage': (13, 20), 'AttackSpeed': 10},
+                          'Price': 2152, 'Damage': (13, 20), 'AttackSpeed': 10},
 
                          {'Name': 'Zweihänder', 'Description': """Legendary great sword made
-                          for elegant and deadly movements.""", 'Price': 1900,
+                          for elegant and deadly movements.""", 'Price': 6288,
                           'Damage': (15, 20), 'AttackSpeed': 15},
 
                          {'Name': 'Sharpened zweihänder', 'Description': """Legendary great
                           sword was improved, well balanced, well sharpened. It was made for
-                          true connoisseur of martial arts.""", 'Price': 2500,
+                          true connoisseur of martial arts.""", 'Price': 12538,
                           'Damage': (17, 25), 'AttackSpeed': 15})
 
 constHammerWeapon = ({'Name': 'Old hammer', 'Description': """It's an old rusty
                       large heavy hammer for forging metals, crushing stones and skulls.""",
-                      'Price': 1000, 'Damage': (15, 20), 'AttackSpeed': 5},
+                      'Price': 2456, 'Damage': (15, 20), 'AttackSpeed': 5},
 
                      {'Name': "Blacksmith's special hammer", 'Description': """It's an large
                       heavy hammer for forging metals, crushing stones and skulls. It's
                       special blacksmith's tool that not everyone can afford.""",
-                      'Price': 1500, 'Damage': (15, 25), 'AttackSpeed': 5},
+                      'Price': 3140, 'Damage': (15, 25), 'AttackSpeed': 5},
 
                      {'Name': 'Zwerg hammer', 'Description': """This hammer has survived
                       to the present day. It belonged to ancient Zwerg race. They was short
                       and very strong human-like creatures with long beards.""",
-                      'Price': 2300, 'Damage': (20, 25), 'AttackSpeed': 10},
+                      'Price': 12785, 'Damage': (20, 25), 'AttackSpeed': 10},
 
                      {'Name': 'Improved Zwerg hammer', 'Description': """This hammer belonged
                       to ancient Zwerg race. Best modern blacksmiths hard working on this
                       deadly weapon and now it's the best decision to kill everything that
-                      can move.""", 'Price': 3000, 'Damage': (20, 30), 'AttackSpeed': 10})
+                      can move.""", 'Price': 18091, 'Damage': (20, 30), 'AttackSpeed': 10})
 
 constBowWeapon = ({'Name': 'Old bow', 'Description': """This bow was sold by old hunter. It
-                   still can shoot, but it's so old as our world!""", 'Price': 500,
+                   still can shoot, but it's so old as our world!""", 'Price': 1879,
                    'Damage': (10, 15), 'AttackSpeed': 10},
 
                   {'Name': 'Long bow', 'Description': """Common bow, good choice to hunt some
-                   bad guys""", 'Price': 1000, 'Damage': (12, 17), 'AttackSpeed': 10},
+                   bad guys""", 'Price': 1959, 'Damage': (12, 17), 'AttackSpeed': 10},
 
                   {'Name': 'Short bow', 'Description': """Common short bow for adventurer who
-                   prefer attack speed to damage.""", 'Price': 1500, 'Damage': (10, 17),
+                   prefer attack speed to damage.""", 'Price': 2279, 'Damage': (10, 17),
                    'AttackSpeed': 15},
 
                   {'Name': 'Elvegr bow', 'Description': """The tale of this great beautiful
                    carved bow is that it was made by Elvegr race master. This bow is the top
                    of skill. It allows shoot fast and deal a lot damage. What can be better
-                   for accurate marksman?""", 'Price': 2100, 'Damage': (17, 20),
+                   for accurate marksman?""", 'Price': 6381, 'Damage': (17, 20),
                    'AttackSpeed': 15})
 
 constAnyWeapon = (constDaggerWeapon, constSwordWeapon, constAxeWeapon, constSpearWeapon,
